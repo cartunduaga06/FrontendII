743e5638f276cd57c5d863c03e7a1eb1c9f05ff5
cartunduaga06 <cartunduaga@gmail.com>
2022-03-15 20:45:19 -0500

    clase 7

--- clase7/script.js
+++ clase7/script.js
@@ -1,25 +1,27 @@
-const imagenes = document.querySelectorAll("img");
-const urlImagenes = [];
+// const imagenes = document.querySelectorAll("img");
+// const urlImagenes = [];
 
-imagenes.forEach((_, index) => {
-  const url = prompt(`Ingresa la url para la imagen ${index + 1}`);
-  urlImagenes.push(url);
+// imagenes.forEach((_, index) => {
+//   const url = prompt(`Ingresa la url para la imagen ${index + 1}`);
+//   urlImagenes.push(url);
   
-});
+// });
 
-console.log(urlImagenes);
+// console.log(urlImagenes);
 
-const contenedor = document.querySelectorAll(".rutas-img");  
+// const contenedor = document.querySelectorAll(".rutas-img");  
 
-urlImagenes.forEach((url, index) => {
-  const nodoImagen = document.querySelector(`#imagen-${index + 1}`);
-  nodoImagen.src = url;
-  contenedor[index].setAttribute("href", url);
-  contenedor[index].setAttribute("target", "_blank");
-});
+// urlImagenes.forEach((url, index) => {
+//   const nodoImagen = document.querySelector(`#imagen-${index + 1}`);
+//   nodoImagen.src = url;
+//   contenedor[index].setAttribute("href", url);
+//   contenedor[index].setAttribute("target", "_blank");
+//
 
-
-
+//ciclo for hasta 1000
+for (let i = 1; i <= 1000; i++) {
+  console.log(i);
+}
 
 
 
---
+++ clase8.md
@@ -0,0 +1,8 @@
+# clase8 eventos
+
+- contador de clicks
+- cronometro
+- consigna de la clase 
+https://docs.google.com/document/d/1WB7XHg_0Q191jJdAoWf5GXxUxfhl1rkl/edit
+
+
